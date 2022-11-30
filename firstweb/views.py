a796d52a1e1ce715ab8366765088accfa91c86d5
vinodh1988 <k.c.vinodh@hotmail.com>
2022-11-30 09:37:49 +0530

    django-app-home

--- firstweb/views.py
+++ firstweb/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from .models import Person
 
 # Create your views here.
+def app_home(request):
+    people = Person.objects.all()
+    context={
+        'people' : people
+    }
+
+    return render(request,'webhome.html',context)
