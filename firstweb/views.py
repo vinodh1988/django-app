ed52acc4d070c24d4ca40c08ecfe01fae693a9b5
vinodh1988 <k.c.vinodh@hotmail.com>
2022-11-30 11:10:09 +0530

    post

--- firstweb/views.py
+++ firstweb/views.py
@@ -1,11 +1,19 @@
 from django.shortcuts import render
 from .models import Person
+from .forms import PersonForm
 
 # Create your views here.
 def app_home(request):
     people = Person.objects.all()
+
+    if request.method == 'POST':
+        form = PersonForm(request.POST)
+        if form.is_valid():
+            form.save()
+    form = PersonForm()
     context={
-        'people' : people
-    }
+        'people' : people,
+         'form': form
+        }
 
     return render(request,'webhome.html',context)
