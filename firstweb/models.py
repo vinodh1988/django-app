d46e0ffee4d15a017f004d20eda69e7add0cbd00
vinodh1988 <k.c.vinodh@hotmail.com>
2022-11-30 08:47:51 +0530

    modelcreation

--- firstweb/models.py
+++ firstweb/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Person(models.Model):
+    sno=models.IntegerField(primary_key=True)
+    name=models.CharField(max_length=30,null=False)
+    city=models.CharField(max_length=30,null=False)
+
+    def __repr__(self):
+        return str({'sno':self.sno,'name':self.name,'city':self.city})
