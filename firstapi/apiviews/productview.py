5b380480db315577d806f2d00300fe2979cb0af6
vinodh1988 <k.c.vinodh@hotmail.com>
2022-11-30 12:54:26 +0530

    post

--- firstapi/apiviews/productview.py
+++ firstapi/apiviews/productview.py
@@ -2,9 +2,24 @@ from rest_framework.decorators import APIView
 from firstapi.models import Product
 from firstapi.serializers import ProductSerializer
 from rest_framework.response import Response
+from rest_framework import status
 
 class ProductAPI(APIView):
     def get(self,request,pk=None):
         products=Product.objects.all()
         result=ProductSerializer(products,many=True)
         return Response(result.data)
+
+    def post(self,request):
+        record=ProductSerializer(data=request.data)
+        try:
+            if(record.is_valid()):
+                record.save()
+                return Response(record.data)
+            else:
+                return Response({'error':'not valid record'},status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        except Exception as e:
+            return Response({'error':'server error'},status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+       
+
+                   
